2cb857c8fb07537658818d4b4d2e674ef806aab5
vinay <itsvinayadav@gmail.com>
2025-02-09 19:35:43 +0530

    enhance error handling for fetching connection requests and connections; add user feed route to exclude hidden users

--- src/routes/user.js
+++ src/routes/user.js
@@ -19,7 +19,7 @@ userRouter.get('/user/requests/received', userAuth, async (req, res) => {
 
         res.status(200).send({ message: "Connection requests fetched successfully!", data: requests });
     } catch (error) {
-        res.status(500).send({ error: "Error creating user", details: error.message });
+        res.status(500).send({ error: "Error fetching connection requests", details: error.message });
     }
 });
 
@@ -42,7 +42,30 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
 
         res.status(200).send({ message: "All connections fetched successfully!", data });
     } catch (error) {
-        res.status(500).send({ error: "Error creating user", details: error.message });
+        res.status(500).send({ error: "Error fetching connections", details: error.message });
+    }
+});
+
+
+userRouter.get('/user/feed', userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const requests = await ConnectionRequest.find({
+            $or: [{ toUserId: loggedInUser._id }, { fromUserId: loggedInUser._id }],
+        }).select('fromUserId toUserId');
+
+        const hideUsersFromFeed = new Set();
+        requests.forEach((req) => {
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString());
+        });
+
+        const data = await User.find({ _id: { $nin: Array.from(hideUsersFromFeed) } }, { _id: { $ne: loggedInUser._id } }).select(USER_SAFE_DATA);
+
+        res.status(200).send({ message: "User feed fetched successfully!", data });
+    } catch (error) {
+        res.status(400).send({ error: "Error fetching user feed", details: error.message });
     }
 });
 
