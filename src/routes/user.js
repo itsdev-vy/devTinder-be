27a18eab402915e50f3018830d1afdf8c712f4d9
vinay <itsvinayadav@gmail.com>
2025-02-09 19:57:29 +0530

    add pagination to user feed route with limit and page parameters

--- src/routes/user.js
+++ src/routes/user.js
@@ -47,9 +47,13 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
 });
 
 
-userRouter.get('/user/feed', userAuth, async (req, res) => {
+userRouter.get('/user/feed?page=1&limit=10', userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user;
+
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
 
         const requests = await ConnectionRequest.find({
             $or: [{ toUserId: loggedInUser._id }, { fromUserId: loggedInUser._id }],
@@ -61,7 +65,12 @@ userRouter.get('/user/feed', userAuth, async (req, res) => {
             hideUsersFromFeed.add(req.toUserId.toString());
         });
 
-        const data = await User.find({ _id: { $nin: Array.from(hideUsersFromFeed) } }, { _id: { $ne: loggedInUser._id } }).select(USER_SAFE_DATA);
+        const data = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(hideUsersFromFeed) } },
+                { _id: { $ne: loggedInUser._id } }
+            ]
+        }).select(USER_SAFE_DATA).skip((page - 1) * limit).limit(limit);
 
         res.status(200).send({ message: "User feed fetched successfully!", data });
     } catch (error) {
